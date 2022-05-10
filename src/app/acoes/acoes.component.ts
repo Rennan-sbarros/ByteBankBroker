739883199cf92e7cce65350d811f5c1d4348e126
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-10 18:44:04 -0300

    feat: unindo fluxos de informações utilizando a função merge

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { merge } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
 
 import { AcoesService } from './acoes.service';
 
@@ -8,10 +10,22 @@ import { AcoesService } from './acoes.service';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent{
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesService.getAcoes();
+  todaAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo inicial');
+    })
+  );
+
+  filtroPorInput$ = this.acoesInput.valueChanges.pipe(
+    tap(() => {
+      console.log('Fluxo do filtro');
+    }),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$ = merge(this.todaAcoes$, this.filtroPorInput$);
 
   constructor(private acoesService: AcoesService) {}
-
 }
