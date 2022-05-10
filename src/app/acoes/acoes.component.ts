54a1ba217764dcb3d1dde54071d9a74eecf4597b
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-10 19:03:16 -0300

    feat: evitando requisições desnecessárias implementando o DebouceTime

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { merge } from 'rxjs';
-import { tap, switchMap, filter } from 'rxjs/operators';
+import { tap, switchMap, filter, debounceTime } from 'rxjs/operators';
 
 import { AcoesService } from './acoes.service';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -15,6 +17,7 @@ export class AcoesComponent {
   todaAcoes$ = this.acoesService.getAcoes().pipe();
 
   filtroPorInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
     filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
     switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
   );
